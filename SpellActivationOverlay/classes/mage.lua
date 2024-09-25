a13e33f822c4b2e65fef3a98e10047e6399ee0dd
class2u <class2u@gmail.com>
2024-09-25 14:09:36 +0800

    更新 法術警告 2.3.2

--- SpellActivationOverlay/classes/mage.lua
+++ SpellActivationOverlay/classes/mage.lua
@@ -14,6 +14,7 @@ local UnitHealth = UnitHealth
 local clearcastingVariants; -- Lazy init in lazyCreateClearcastingVariants()
 
 local arcaneMissiles = 5143;
+local fireBlast = 2136;
 local pyroblast = 11366; -- Pyroblast, the base Pyro spell
 local pyroblastBang = 92315; -- Pyroblast!, a specific spell for instant Pyro introduced in Cataclysm
 
@@ -32,6 +33,7 @@ local HotStreakHandler = {}
 -- Initialize constants
 HotStreakHandler.init = function(self, talentName)
     local fire_blast = { 2136, 2137, 2138, 8412, 8413, 10197, 10199, 27078, 27079, 42872, 42873 }
+    local fire_blast_sod = { 400618, 400619, 400616, 400620, 400621, 400622, 400623 } -- Improved by Overheat rune
     local fireball = { 133, 143, 145, 3140, 8400, 8401, 8402, 10148, 10149, 10150, 10151, 25306, 27070, 38692, 42832, 42833 }
     local frostfire_bolt = { 44614, 47610 }
     local frostfire_bolt_sod = { 401502 }
@@ -41,6 +43,7 @@ HotStreakHandler.init = function(self, talentName)
     local living_bomb_sod = { 401731 }
     local scorch = { 2948, 8444, 8445, 8446, 10205, 10206, 10207, 27073, 27074, 42858, 42859 }
     local pyroblast_cata = { pyroblast }
+    local balefire_bolt_sod = { 428878 }
 
     self.spells = {}
     local function addSpellPack(spellPack)
@@ -49,6 +52,7 @@ HotStreakHandler.init = function(self, talentName)
         end
     end
     addSpellPack(fire_blast);
+    addSpellPack(fire_blast_sod);
     addSpellPack(fireball);
     addSpellPack(frostfire_bolt);
     addSpellPack(frostfire_bolt_sod);
@@ -59,6 +63,7 @@ HotStreakHandler.init = function(self, talentName)
         addSpellPack(living_bomb);
         addSpellPack(living_bomb_sod);
     end
+    addSpellPack(balefire_bolt_sod);
 
     local _, _, tab, index = SAO:GetTalentByName(talentName);
     if (tab and index) then
@@ -86,7 +91,7 @@ end
 HotStreakHandler.hasHotStreakTalent = function(self)
     if (SAO.IsSoD()) then
         -- Not really a 'talent' but in Season of Discovery, hot streak comes from a rune
-        return C_Engraving and C_Engraving.IsRuneEquipped(400624);
+        return C_Engraving and C_Engraving.IsRuneEquipped(48202);
     end
 
     -- Talent information could not be retrieved for Hot Streak
@@ -528,6 +533,26 @@ local function lazyCreateClearcastingVariants(self)
     });
 end
 
+local function useImpact()
+    local impactBuff = 64343;
+    local impactTalent = 11103;
+
+    SAO:CreateEffect(
+        "impact",
+        SAO.WRATH + SAO.CATA,
+        impactBuff,
+        "aura",
+        {
+            talent = impactTalent,
+            requireTalent = true,
+            action = fireBlast,
+            actionUsable = true,
+            overlay = { texture = "lock_and_load", position = "Top" },
+            button = fireBlast,
+        }
+    );
+end
+
 local function useArcaneMissiles()
     local arcaneMissilesBuff = 79683; -- Cataclysm requires a buff before casting Arcane Missiles
 
@@ -556,7 +581,7 @@ end
 
 local function registerClass(self)
     -- Fire Procs
-    self:RegisterAura("impact", 0, 64343, "lock_and_load", "Top", 1, 255, 255, 255, true, { (GetSpellInfo(2136)) });
+    useImpact();
     if self.IsWrath() then
         self:RegisterAura("firestarter", 0, 54741, "impact", "Top", 0.8, 255, 255, 255, true, { (GetSpellInfo(2120)) }); -- May conflict with Impact location
     end
@@ -672,9 +697,6 @@ local function loadOptions(self)
     local firestarterBuff = 54741;
     local firestarterTalent = 44442;
 
-    local impactBuff = 64343;
-    local impactTalent = 11103;
-
     local brainFreezeBuff = 57761;
     local brainFreezeTalent = 44546;
     local brainFreezeSoDRune = 400731;
@@ -692,7 +714,6 @@ local function loadOptions(self)
 
     local arcaneExplosion = 1449;
     local flamestrike = 2120;
-    local fireBlast = 2136;
     local fireball = 133;
     local frostfireBolt = 44614;
     local frostfireBoltSoD = 401502;
@@ -740,7 +761,6 @@ local function loadOptions(self)
         self:AddOverlayOption(hotStreakTalent, hotStreakHeatingUpBuff, 0, hotStreakHeatingUpDetails);
     end
     self:AddOverlayOption(firestarterTalent, firestarterBuff);
-    self:AddOverlayOption(impactTalent, impactBuff);
     if self.IsSoD() then
         self:AddOverlayOption(fingersOfFrostSoDTalent, fingersOfFrostSoDBuff, 0, nil, nil, 2); -- setup any stacks, test with 2 stacks
     elseif self.IsWrath() then
@@ -773,9 +793,6 @@ local function loadOptions(self)
     end
     if self.IsWrath() then
         self:AddGlowingOption(firestarterTalent, firestarterBuff, flamestrike);
-    end
-    if not self.IsEra() then -- Must exclude this option specifically for Classic Era, because the talent exists in Era but the proc is passive
-        self:AddGlowingOption(impactTalent, impactBuff, fireBlast);
     end
     if self.IsSoD() then
         self:AddGlowingOption(brainFreezeSoDRune, brainFreezeSoDBuff, fireball);
