087bcbf03df9ddae565220bac9774d56b8989998
class2u <class2u@gmail.com>
2024-10-02 15:25:46 +0800

    新增 副本戰利品查詢 v4.1.0 + SoD v0.9.59 修正版

---
+++ AtlasLootClassic/CHANGELOG.md
@@ -0,0 +1,23 @@
+# [v4.1.0](https://github.com/snowflame0/AtlasLootClassic_Cata/tree/v4.1.0) (2024-09-30)
+
+[Full Changelog](https://github.com/snowflame0/AtlasLootClassic_Cata/compare/v4.0.1...v4.1.0)
+
+## Highlights
+
+ Fix indentation/trailing whitespace 
+
+## Commits
+
+Adal (1):
+
+- Coren nerfed
+
+Snowflame (2):
+
+- Fix indentation/trailing whitespace
+- Integrating changes by Firsthunterz
+
+iZcr3am (1):
+
+- Translations Update data-cata.lua
+
---
+++ AtlasLootClassic_PvP/Locales/constants.tw.lua
@@ -0,0 +1,36 @@
+﻿local AL = _G.AtlasLoot.GetLocales("zhTW")
+
+if not AL then return end
+
+-- These localization strings are translated on Curseforge: https://www.curseforge.com/wow/addons/atlaslootclassic/localization
+-- PvP
+AL["|cff33ff99Rank:|r %s"] = "|cff33ff99等級:|r %s"
+AL["Arena"] = "競技場"
+AL["Battlegrounds"] = "戰場"
+AL["Class Sets"] = "職業套裝"
+AL["Honor"] = "榮譽"
+AL["Reputation"] = "聲望"
+AL["Sets"] = "套裝"
+AL["Insignia"] = "徽記"
+AL["Non Set '%s'"] = "非套裝 '%s'"
+AL["One-Handed"] = "單手"
+AL["Open World"] = "開放世界"
+AL["PvP Ranks"] = "PvP 等級"
+AL["Ranged"] = "遠程"
+AL["Season %s"] = "賽季 %s"
+AL["Two-Handed"] = "雙手"
+AL["Gladiator Mount"] = "鬥士坐騎"
+AL["Gems"] = "寶石"
+AL["Enhancements"] = "附魔"
+AL["Recipe"] = "設計圖"
+AL["Heirloom"] = "傳家寶"
+AL["SoD Exclusives"] = "探索賽季專屬"
+AL["SoD Exclusive: Phase 1"] = "探索賽季專屬：階段1"
+
+-- 自行加入
+AL["Level 60 Rank 10 Sets"] = "等級 60 10級 套裝" 
+AL["Level 50 Blood Moon"] = "等級 50 血月" 
+AL["Level 40 Blood Moon"] = "等級 40 血月" 
+AL["Level 25 PvP Gear"] = "等級 25 PvP 裝備" 
+AL["Alliance"] = "聯盟" 
+AL["Horde"] = "部落" 
